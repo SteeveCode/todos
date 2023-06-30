7b043f5dca98295cdf7456228aa67bc0ed590284
steevecode <stevecode@yahoo.com>
2023-06-30 17:55:35 +0100

    Step 04 - Enhancing Spring MVC Controller to provide HTML response

--- src/main/java/com/brexson/todos/hello/SayHelloController.java
+++ src/main/java/com/brexson/todos/hello/SayHelloController.java
@@ -16,6 +16,20 @@ public class SayHelloController {
     public String sayHello() {
         return "Hello! What are you learning today?";
     }
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My First HTML Page - Changed</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body - Changed");
+        sb.append("</body>");
+        sb.append("</html>");
 
 
+        return sb.toString();
+    }
 }
