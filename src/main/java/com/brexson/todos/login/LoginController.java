442bea496ef038bf69f33a527455202c841c87a9
steevecode <stevecode@yahoo.com>
2023-07-05 08:48:03 +0100

    Step 08 - Capturing QueryParams using RequestParam and First Look at Model [used Thymeleaf instead]

--- src/main/java/com/brexson/todos/login/LoginController.java
+++ src/main/java/com/brexson/todos/login/LoginController.java
@@ -1,13 +1,19 @@
 package com.brexson.todos.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    //http://localhost:8080/login?name=Ranga
+    //Model
     @RequestMapping("login")
-    public String gotoLoginPage() {
+    public String gotoLoginPage(@RequestParam String name, ModelMap model) {
+        model.put("name", name);
+        System.out.println("Request param is " + name); //NOT RECOMMENDED FOR PROD CODE
         return "login";
     }
 }
