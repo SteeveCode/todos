8376edfed800df2478c30ac7b7b1bb767bd35380
steevecode <stevecode@yahoo.com>
2023-07-05 21:09:48 +0100

    Step 20 - Lets Add a New Todo - Create a new View [View Tech is Thymeleaf]

--- src/main/java/com/brexson/todos/todo/TodoController.java
+++ src/main/java/com/brexson/todos/todo/TodoController.java
@@ -3,8 +3,11 @@ package com.brexson.todos.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -23,6 +26,17 @@ public class TodoController {
         model.addAttribute("todos", todos);
 
         return "listTodos";
+    }
+    //GET, POST
+    @GetMapping("add-todo")
+    public String showNewTodoPage() {
+        return "todo";
+    }
+
+    @PostMapping("add-todo")
+    public String addNewTodo(@RequestParam String description, ModelMap model) {
+
+        return "redirect:list-todos";
     }
 
 }
--- src/main/java/com/brexson/todos/todo/TodoService.java
+++ src/main/java/com/brexson/todos/todo/TodoService.java
@@ -10,16 +10,18 @@ import java.util.List;
 public class TodoService {
 
     private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
 
     static {
-        todos.add(new Todo(1, "in28ms","Learn AWS",
+        todos.add(new Todo(++todosCount, "in28ms","Learn AWS",
                 LocalDate.now().plusYears(1), false ));
-        todos.add(new Todo(2, "in28ms","Learn DevOps",
+        todos.add(new Todo(++todosCount, "in28ms","Learn DevOps",
                 LocalDate.now().plusYears(2), false ));
-        todos.add(new Todo(3, "in28ms","Learn Full Stack Development",
+        todos.add(new Todo(++todosCount, "in28ms","Learn Full Stack Development",
                 LocalDate.now().plusYears(3), false ));
     }
     public List<Todo> findByUsername(String username){
         return todos;
     }
+
 }
