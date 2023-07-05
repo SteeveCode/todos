06331aa949003228c58b89f85b46f92212024774
steevecode <stevecode@yahoo.com>
2023-07-05 11:36:42 +0100

    Step 12 - Displaying Login Credentials in a JSP using Model [used Thymeleaf instead]

--- src/main/java/com/brexson/todos/login/LoginController.java
+++ src/main/java/com/brexson/todos/login/LoginController.java
@@ -2,6 +2,8 @@ package com.brexson.todos.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,8 +12,13 @@ public class LoginController {
 
     //http://localhost:8080/login?name=Ranga
     //Model
-    @RequestMapping("login")
+    @GetMapping("login")
     public String gotoLoginPage() {
         return "login";
+    }
+    @PostMapping("login")
+    public String gotoWelcomePage(@RequestParam String name, ModelMap model) {
+        model.put("name",name);
+        return "welcome";
     }
 }
