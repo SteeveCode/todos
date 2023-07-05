3ad97f40ebfa684bb8ecb9c2fff2d64db81f6828
steevecode <stevecode@yahoo.com>
2023-07-05 22:50:23 +0100

    Step 22 - Adding Validations using Spring Boot Starter Validation [View Tech is Thymeleaf]

--- src/main/java/com/brexson/todos/todo/TodoController.java
+++ src/main/java/com/brexson/todos/todo/TodoController.java
@@ -29,14 +29,17 @@ public class TodoController {
     }
     //GET, POST
     @GetMapping("add-todo")
-    public String showNewTodoPage() {
+    public String showNewTodoPage(ModelMap model) {
+        String username = (String)model.get("name");
+        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+        model.put("todo", todo);
         return "todo";
     }
 
     @PostMapping("add-todo")
-    public String addNewTodo(@RequestParam String description, ModelMap model) {
+    public String addNewTodo(ModelMap model, Todo todo) {
         String username = (String)model.get("name");
-        todoService.addTodo(username, description,
+        todoService.addTodo(username, todo.getDescription(),
                 LocalDate.now().plusYears(1), false);
         return "redirect:list-todos";
     }
