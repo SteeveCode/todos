cda6b182ae80fbf94c1c3b8020dd012390d0ea27
steevecode <stevecode@yahoo.com>
2023-07-21 23:41:14 +0100

    Section 04: Step 15 - Adding Update Todo and Create Todo REST API to Spring Boot Backend API

--- src/main/java/com/brexson/todos/todo/TodoResource.java
+++ src/main/java/com/brexson/todos/todo/TodoResource.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 public class TodoResource {
-
     private TodoService todoService;
     public TodoResource(TodoService todoService) {
         this.todoService = todoService;
@@ -31,5 +30,15 @@ public class TodoResource {
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
         todoService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+    @PutMapping("/users/{username}/todos/{id}")
+    public Todo updateTodo(@PathVariable String username, @PathVariable int id, @RequestBody Todo todo) {
+        todoService.updateTodo(todo);
+        return todo;
+    }
+    @PostMapping("/users/{username}/todos")
+    public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        return createdTodo;
     }
 }
